41b14a2d2afaaa7bcdc26e87b4cfdd3c2ad54e06
bong <>
2026-04-03 16:47:53 +0900

    bridge test

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'dart:ui' as html;
+
 import 'package:flutter/material.dart';
+import 'package:js/js_util.dart' as js_util;
 import 'package:papa/Constants.dart';
 
 import 'PapaComm.dart';
+import 'common/GradientButtonStyle.dart';
 import 'l10n/app_localizations.dart';
 
 void main() {
@@ -15,6 +19,23 @@ class PapaApp extends StatefulWidget {
   State<PapaApp> createState() => PapaAppState();
 }
 
+void registerNativeHandler(void Function(String message) onMessage) {
+  js_util.setProperty(
+    html.window,
+    'onNativeMessage',
+        (String message) {
+      onMessage(message);
+    },
+  );
+}
+
+void sendAndroid(String message) {
+  final bridge = js_util.getProperty(html.window, 'AndroidBridge');
+  if (bridge != null) {
+    js_util.callMethod(bridge, 'postMessage', [message]);
+  }
+}
+
 class PapaAppState extends State<PapaApp> {
   Locale locale = const Locale(Constants.KO);
 
@@ -26,6 +47,12 @@ class PapaAppState extends State<PapaApp> {
   @override
   void initState() {
     super.initState();
+/*
+    registerNativeHandler((message) {
+      print('from android: $message');
+    });
+
+ */
   }
 
   @override
@@ -49,6 +76,35 @@ class PapaAppState extends State<PapaApp> {
                   maxLines: 1,
                   overflow: TextOverflow.ellipsis,
                 ),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  color: Colors.white,
+                  width: double.infinity,
+                  child: GradientButton(
+                    onPressed: () {
+                      sendAndroid('test');
+                    },
+                    style: GradientButtonStyle(
+                      padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      textStyle: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w900,
+                        color: Colors.white,
+                      ),
+                      elevation: 0,
+                    ),
+                    gradient: LinearGradient(
+                      colors: [Color(0xff1D36F3), Color(0xff1D36F3)],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ),
+                    child: Text('send android'),
+                  ),
+                ),
+
               ],
             ),
           );

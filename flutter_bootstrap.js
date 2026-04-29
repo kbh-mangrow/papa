f5084d3740e0809b6962d30699958e2d58d43611
kbh-mangrow <kbh-mangrow@users.noreply.github.com>
2026-04-29 04:30:28 +0000

    deploy: a76de8a8386698794e3234d6b0845da5bff38860

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"39d6d6e699e51b2874210e14cddf1a22fb9524
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "724712155"
+    serviceWorkerVersion: "3630918555"
   }
 });
